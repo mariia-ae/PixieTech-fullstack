ff4f5f92d89ad5e5c9d576936d1ff06b9778b9a8
MariiaCh21 <chertkovamasha21@icloud.com>
2026-02-19 13:15:06 +0100

    fix v2

--- build.sh
+++ build.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 set -o errexit
-
+python -m pip install --upgrade pip
 pip install -r requirements.txt
-
 cd BackendPixieTech
-
 python manage.py collectstatic --noinput
 python manage.py migrate
