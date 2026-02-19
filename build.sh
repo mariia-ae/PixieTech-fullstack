a474cbc9e33a7661084f4dcdfef387129ba5339c
MariiaCh21 <chertkovamasha21@icloud.com>
2026-02-19 13:23:31 +0100

    fix x

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -o errexit
 python -m pip install --upgrade pip
+pip install whitenoise==6.6.0
 pip install -r requirements.txt
 cd BackendPixieTech
 python manage.py collectstatic --noinput
